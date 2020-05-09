3d5a398a2554495699d3124c8193678ac2e6009b
Maciej Pastor <maciej.pastor@gmail.com>
2020-05-10 00:18:06 +0200

    Test przed pracą nad projektem 09.05.2020 - jutro ostatnie zajęcia ze "strukturalki"

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,5 +1,23 @@
 public class GameEngine {
     public static void main(String[] args) {
-        System.out.println(Weather.values());
+        for (Weather a : Weather.values()) {
+            System.out.println(a);
+        }
+        Weather.SUN.setLevel(3);
+        Weather.FERTILIZER.setLevel(4);
+        Weather.WATER.setLevel(2);
+        Weather.WORM.setLevel(4);
+
+        System.out.println();
+
+         for (Weather b : Weather.values()) {
+        System.out.println(b + " " + b.level);
+         }
+
+        System.out.println(Weather.WORM + " " + Weather.WORM.level);
+        int a = 1;
+        Weather.WORM.setLevel(a);
+
+        System.out.println(Weather.WORM + " " + Weather.WORM.level);
     }
 }
