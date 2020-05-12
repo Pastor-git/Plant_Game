6e2128844b37827a420af8bf204dfc41b92de596
Maciej Pastor <maciej.pastor@gmail.com>
2020-05-12 18:47:41 +0200

    Test przed pracą nad projektem 09.05.2020 - jutro ostatnie zajęcia ze "strukturalki"

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,15 +1,37 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GameEngine {
+public class GameEngine implements Decisions {
+
+
+
+
+    @Override
+    public void addwater() {
+
+    }
+
+    @Override
+    public void killworm() {
+
+    }
+
+    @Override
+    public void adfertilizer() {
+
+    }
+
 
     public static void main(String[] args) {
 
         Random randomGenerator = new Random();
         Scanner ruch = new Scanner(System.in);
 
-        Plant plant1 = new Plant("kwiatek" , true);
+        Plant plant1 = new Plant("kwiatek", true);
         System.out.println(plant1.name + " czy żyje?: " + plant1.life);
+        System.out.println("Nazwa Twojej roślinki: " + plant1.name);
+        System.out.println();
+        System.out.println("Warunki startowe: ");
 
         int a = randomGenerator.nextInt(5);
         Weather.SUN.setLevel(a);
@@ -20,28 +42,17 @@ public class GameEngine {
         int d = randomGenerator.nextInt(3);
         Weather.WORM.setLevel(d);
 
+        for (Weather lvl : Weather.values()) {
+            System.out.println(lvl + " " + lvl.level);
+        }
         System.out.println();
+        System.out.println("Wybierz działanie: ");
+        System.out.println("Dodaj wody - 1");
+        System.out.println("Dodaj nawozu - 2");
+        System.out.println("Zabij robaki - 3");
 
-         for (Weather lvl : Weather.values()) {
-        System.out.println(lvl + " " + lvl.level);
-         }
-
-
-        System.out.println();
-
-
-
-//        public void addwater();
-//        public void killworm();
-//        public void adfertilizer();
-
-
-        System.out.println("dodaj wody - 1");
-        System.out.println("dodaj nawozu - 2");
-        System.out.println("zabij robaki - 3");
-
-
-
+        Weather.WORM.level = Weather.WORM.level + 2;
+        System.out.println(Weather.WORM.name() + " " + Weather.WORM.level);
 
     }
 }
