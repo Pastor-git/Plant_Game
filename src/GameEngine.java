76e7a338413c4b96a0b39f047ac6bd0d0b1ed890
Maciej Pastor <maciej.pastor@gmail.com>
2020-05-12 18:30:12 +0200

    Test przed pracą nad projektem 09.05.2020 - jutro ostatnie zajęcia ze "strukturalki"

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,23 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GameEngine {
+
     public static void main(String[] args) {
-        for (Weather a : Weather.values()) {
-            System.out.println(a);
-        }
-        Weather.SUN.setLevel(3);
-        Weather.FERTILIZER.setLevel(4);
-        Weather.WATER.setLevel(2);
-        Weather.WORM.setLevel(4);
+
+        Random randomGenerator = new Random();
+        Scanner ruch = new Scanner(System.in);
+
+        Plant plant1 = new Plant("kwiatek" , true);
+        System.out.println(plant1.name + " czy żyje?: " + plant1.life);
+
+        int a = randomGenerator.nextInt(5);
+        Weather.SUN.setLevel(a);
+        int b = randomGenerator.nextInt(5);
+        Weather.FERTILIZER.setLevel(b);
+        int c = randomGenerator.nextInt(5);
+        Weather.WATER.setLevel(c);
+        int d = randomGenerator.nextInt(3);
+        Weather.WORM.setLevel(d);
 
         System.out.println();
 
-         for (Weather b : Weather.values()) {
-        System.out.println(b + " " + b.level);
+         for (Weather lvl : Weather.values()) {
+        System.out.println(lvl + " " + lvl.level);
          }
 
-        System.out.println(Weather.WORM + " " + Weather.WORM.level);
-        int a = 1;
-        Weather.WORM.setLevel(a);
 
-        System.out.println(Weather.WORM + " " + Weather.WORM.level);
+        System.out.println();
+
+
+
+//        public void addwater();
+//        public void killworm();
+//        public void adfertilizer();
+
+
+        System.out.println("dodaj wody - 1");
+        System.out.println("dodaj nawozu - 2");
+        System.out.println("zabij robaki - 3");
+
+
+
+
     }
 }
